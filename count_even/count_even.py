be72724c2b4310b773ccbf5613af2e9631572a3f
Chris P <sahib@online.de>
2013-05-29 18:13:03 +0200

    Corrected typo in description

--- count_even/count_even.py
+++ count_even/count_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Die count_event soll die Anzahler gerader Zahlen in einer Liste zählen.
+# Die count_event soll die Anzahl gerader Zahlen in einer Liste zählen.
 # Allerdings enthält sie momentan einen Fehler. Wo?
 #
 # Wenn ihr wirklich nicht drauf kommt könnt ihr auch git log/diff/show nutzen
