f15fe898da833e1710337a5cbc88ba473b027d4a
Chris P <sahib@online.de>
2013-05-31 00:31:43 +0200

    Fixed silly bug in deduplicate

--- deduplicate/deduplicate.py
+++ deduplicate/deduplicate.py
@@ -11,7 +11,5 @@ def deduplicate(data):
     for element in data:
         if not element in no_dupes:
             no_dupes.append(element)
-        else:
-            no_dupes.insert(0, element)
 
     return no_dupes
