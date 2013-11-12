306836e298cc9f608d944192814e24ae419c56a0
Chris P. <sahib@online.de>
2013-11-12 10:15:11 +0100

    Update monte_carlo_pi.py

--- monte_carlo_pi/monte_carlo_pi.py
+++ monte_carlo_pi/monte_carlo_pi.py
@@ -17,6 +17,6 @@ from math import sqrt
 def monte_carlo_pi(num_iterations=10000000):
     hits = 0
     for _ in range(num_iterations):
-        hits += sqrt(random() ** 2 + random() ** 2) > 1
+        hits += sqrt(random() ** 2 + random() ** 2) < 1
 
     return (float(hits) / num_iterations) * 4
